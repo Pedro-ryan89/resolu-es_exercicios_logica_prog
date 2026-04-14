1c79e81278a303344a0bf8953165719cf0f8e9cf
Pedro-ryan89 <abrobra05@gmail.com>
2026-04-14 20:52:29 -0300

    terceira lista de questões

--- Listas_exercicios_IP/Pedro_Ryan_Coelho_do_Nascimento_Lista3.py
+++ Listas_exercicios_IP/Pedro_Ryan_Coelho_do_Nascimento_Lista3.py
@@ -92,12 +92,66 @@ elif votos_cand_1 == votos_cand_2 or votos_cand_2 == votos_cand_1:
     print("empate")
 """ 
  
+"""
 # questão 5
 numero = int(input("numero: "))
 
 if numero >= 1 and numero <=4:
-    n =  (numero / 2)) +  (4 * numero) / 7
+    n =  (((numero / 2) +  (4 * numero) / 7) * 5) ** 3
     print(n)
-#elif numero >= 5 and numero <=9:
-#    n = ()
+elif numero >= 5 and numero <=9:
+    n = (((numero / 3) + (numero /9) * 2) *3) ** 2 
+    print(n)
+else:
+    n = (4 * ((3 * numero + (2 * numero / 7)) /2 )) ** 2
+    print(n)
     
+"""
+
+"""
+# questão 6
+
+ang_1 = int(input("primeiro angulo: "))
+ang_2 = int(input("segundo angulo: "))
+ang_3 = int(input("terceiro angulo: "))
+
+resultado = ang_1 + ang_2 + ang_3
+
+if resultado !=  180:
+    print("nao e triangulo")
+else:
+    if ang_1 == ang_2 and ang_1 == ang_3:
+        print("equilatero")
+    elif ang_1 != ang_2 and ang_1 != ang_3 and ang_2 != ang_3:
+        print("escaleno")
+    else:
+        print("isosceles")    
+   
+   
+"""
+        
+# questão 7
+
+"""
+voce = input("amigo ou inimigo de joao: ")
+janio = input("janio e amigo ou inimigo: ")
+
+if voce == janio:
+    print("janio e meu amigo")
+else:
+    print("janio e meu inimigo")
+"""
+
+
+# questão 8
+
+horario_segundo = int(input("insira um horaria entre 0 - 43200: "))
+
+minutos_ang = horario_segundo * 0.1
+horas_ang = horario_segundo / 120
+
+menor_angulo = (minutos_ang - horas_ang) % 360
+
+if menor_angulo > 180:
+    menor_angulo = 360 - menor_angulo  
+print(f"Angulo: {menor_angulo} graus")
